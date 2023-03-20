4a5bd9731211575a1b58986318a01bffd7cf670e
Welerson Silva <welerson.cote2@gmail.com>
2023-03-19 21:21:09 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturasDecisãoRepetição1/Program.cs
@@ -0,0 +1,21 @@
+﻿const string APROVADO = "Aluno Aprovado!";
+const string REPROVADO = "Aluno Reprovado!";
+const string RECUPERACAO = "Aluno ficou de Recuperação!";
+
+Console.WriteLine("Digite uma nota para o Aluno:");
+
+decimal nota = Convert.ToDecimal(Console.ReadLine());
+
+if (nota >= 10)
+{
+    Console.WriteLine(APROVADO);
+}
+else if (nota >= 4 && nota < 5) //PRATICAR ESSA ESTRUTURA DE CÓDIGO PARA MELHOR FIXAÇÃO
+{
+    Console.WriteLine(RECUPERACAO);
+}
+else
+{
+    Console.WriteLine(REPROVADO);
+}
+
